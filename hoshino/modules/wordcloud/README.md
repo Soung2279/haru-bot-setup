ff5b48adb954aa0088961341125e76ea2437c2c9
SYoung <67218202+Soung2279@users.noreply.github.com>
2021-09-19 03:01:34 +0800

    更新插件：词云，修复词云生成功能。需自行更改词云图片存储路径

--- hoshino/modules/wordcloud/README.md
+++ hoshino/modules/wordcloud/README.md
@@ -1,5 +1,14 @@
 # wordcloud-hoshino
-  头一次写代码 写的不是很好<br>
-  使用前请先确保你的gocq版本允许保存聊天记录
-  并安装wordcloud库和jieba库<br>
+感谢othinus001做出的指令优化和功能添加，终于可以分群查询词云了<br> 
+指令 `生成今日词云` `生成昨日词云`可以查询本群词云，<br>
+指令`查询某月某日词云`可以查询全服词云
+可以查询单独群内的词云也可以查询公共的词云<br> 
+对指令，单字，语气词进行剔除，如需增加不需要的词汇请在tyc.txt自己新开一行加<br> 
+使用前请先确保你的gocq允许保存聊天记录（info)<br> 
+并安装wordcloud库和jieba库<br>
+## 部署方法<br>
+1.新建一个文件夹wordcloud在里面`git clone https://github.com/erweixi/wordcloud-hoshino.git`<br>
+2.修改里面路径和QQ号<br>
+3.将tyc.txt和ttf字体文件丢进load_path并安装字体<br> 
+tyc里是不想要的词，可以自己一行一行加<br> 
 
--- hoshino/modules/wordcloud/cloud.py
+++ hoshino/modules/wordcloud/cloud.py
@@ -1,49 +1,42 @@
+from posixpath import supports_unicode_filenames
+from time import time
+import time
+import datetime
 import jieba
-import re
+import re, os, shutil
 import nonebot
 import wordcloud
+import random
 import hoshino
 from hoshino.typing import CQEvent
-from hoshino import Service,R,priv
+from hoshino import Service, priv
 from nonebot import MessageSegment,NoticeSession
 import base64
-from PIL import Image
 import numpy as np
-import datetime
-import shutil
-import os
-
+from PIL import Image
+import matplotlib.pyplot as plt
 
 sv_help = '''
-查询(.*)月(\d+)日词云
-生成今日词云
+- [生成今日词云]  看看今天的群聊词云
+- [生成昨日词云]  看看昨天的群聊词云
+- [检查词云]
+- [清理词云]
 '''.strip()
 
 sv = Service(
     name = '词云',  #功能名
     use_priv = priv.NORMAL, #使用权限   
     manage_priv = priv.ADMIN, #管理权限
-    visible = True, #False隐藏
-    enable_on_default = True, #是否默认启用
-    bundle = '通用', #属于哪一类
-    help_ = sv_help #帮助文本
+    visible = True, #可见性
+    enable_on_default = True, #默认启用
+    bundle = '通用', #分组归类
+    help_ = sv_help #帮助说明
     )
 
-@sv.on_fullmatch(["帮助词云"])
-async def bangzhu_wordcloud(bot, ev):
-    await bot.send(ev, sv_help, at_sender=True)
-
-loadpath = 'G:/Gomirai/logs/'#此处填gocq的logs路径
+loadpath = 'G:\\Gomirai\\logs\\'#此处填gocq的logs路径
 self_id = '756160433'#此处填机器人的QQ号
-load_in_path = 'G:/Gomirai/wordcloud/'#此处填词云图片保存的路径
-
-
-@sv.on_rex(f'^查询(.*)月(\d+)日词云$')
-async def ciyun(bot, ev: CQEvent):
-    match = ev['match']
-    month = int(match.group(1))
-    day = int(match.group(2))
-    await bot.send(ev,MessageSegment.image(f'file:///{load_in_path}//2021-{month:02}-{day:02}.png'))
+load_in_path = 'G:\\Gomirai\\wordcloud'#此处填词云图片保存的路径
+wd_img = 'G:\\qqbot\\hoshino\\modules\\wordcloud\\wd.png' #此处填词云的背景图片
 
 @nonebot.scheduler.scheduled_job(
     'cron',
@@ -58,25 +51,76 @@ async def makecloud():
     except Exception as e:
         today = datetime.date.today().__format__('%Y-%m-%d')
         await bot.send_private_msg(user_id=hoshino.config.SUPERUSERS[2], message=f'{today}词云生成失败,失败原因:{e}')
+        
 
-@sv.on_fullmatch('生成今日词云')
-async def getciyun(bot,ev):
-    if not hoshino.priv.check_priv(ev, hoshino.priv.SUPERUSER):
-        return
-    await bot.send(ev,message = 'execute',at_sender = True)
-    makeclouds()
+    
+def random_color_func(word=None, font_size=None, position=None,
+                      orientation=None, font_path=None, random_state=None):
+  
+    if random_state is None:
+        random_state = Random()
+    return "hsl(%d, 75%%, 62%%)" % random_state.randint(0, 225)#值，饱和度，色相
+    
+def del_files(path):
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name.endswith(".png"):
+                os.remove(os.path.join(root,name))
+                print("删除词云文件：" + os.path.join(root,name))
 
-
-
-def makeclouds():
+def makeclouds(gid):
     global loadpath
+    global load_in_path
     bot = nonebot.get_bot()
     today = datetime.date.today().__format__('%Y-%m-%d')
     f = open(loadpath + f"\\{today}.log", "r", encoding="utf-8")
     f.seek(0)
+    gida = str(gid)
     msg=''
     for line in f.readlines():          #删除前缀和自己的发言
-        if self_id in line:
+        if self_id in line or gida not in line:
+            continue
+        try:                         
+            o = line.split("的消息: ")[1]
+            msg += o  
+        except:
+            pass
+    msg = re.sub('''[a-zA-Z0-9'!"#$%&\'()*+,-./:;<=>?@，。?★、…【】《》？“”‘'！[\\]^_`{|}~\s]+''', "", msg)
+    msg = re.sub('[\001\002\003\004\005\006\007\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a]+', '', msg)
+    banword = []#此处为不显示的删除禁词
+    ls = jieba.lcut(msg,cut_all=True)#制作分词
+    stopwords = set()
+    content = [line.strip() for line in open(load_in_path+f"\\tyc.txt",\
+               encoding='utf-8').readlines()]
+    stopwords.update(content)
+    txt = " ".join(ls)
+    bg_pic=np.array(Image.open(wd_img))
+    w = wordcloud.WordCloud(mask=bg_pic,font_path=load_in_path+f"\\SimHei.ttf",\
+                            max_words=10000, width=1000, height=700,\
+                            background_color='white',stopwords=stopwords,\
+                            relative_scaling=0.5,min_word_length=2,\
+                            color_func=random_color_func#调色
+        )
+    w.generate(txt)
+    del_files(load_in_path)
+    w.to_file(f"{today}.png")
+    shutil.move(f"{today}.png",load_in_path)
+    srcFile = f"{load_in_path}\\{today}.png"
+    dstFile = f"{load_in_path}\\{today}-{gid}.png"
+    os.rename(srcFile,dstFile)
+        
+def makecloudsb(gid):
+    global loadpath
+    global load_in_path
+    bot = nonebot.get_bot()
+    today = datetime.date.today().__format__('%Y-%m-%d')
+    yesterday = (datetime.date.today() + datetime.timedelta(-1)).__format__('%Y-%m-%d')
+    gida = str(gid)
+    f = open(loadpath + f"\\{yesterday}.log", "r", encoding="utf-8")
+    f.seek(0)
+    msg=''
+    for line in f.readlines():          #删除前缀和自己的发言
+        if self_id in line or gida not in line:
             continue
         try:                         
             o = line.split("的消息: ")[1]
@@ -87,16 +131,90 @@ def makeclouds():
     msg = re.sub('[\001\002\003\004\005\006\007\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a]+', '', msg)
     banword = []#此处为不显示的删除禁词
     ls = jieba.lcut(msg)#制作分词
+    stopwords = set()
+    content = [line.strip() for line in open(load_in_path+f"\\tyc.txt",encoding='utf-8').readlines()]
+    stopwords.update(content)
     txt = " ".join(ls)
-    w = wordcloud.WordCloud( \
-        width = 1000, height = 618,\
-        max_words = 114514,\
-        background_color = "white",regexp="\d*"
+    bg_pic=np.array(Image.open(wd_img))
+    w = wordcloud.WordCloud(mask=bg_pic,font_path=load_in_path+f"\\SimHei.ttf",\
+                            max_words=10000, width=1000, height=700,\
+                            background_color='white',stopwords=stopwords,\
+                            relative_scaling=0.5,min_word_length=2,\
+                            color_func=random_color_func#词汇上限，宽，高,背景颜色去除停用词(tyc.txt),频次与大小相关度，最小词长,调色
         )
-    w.generate(msg)
-    w.to_file(f"{today}.png")
-    try:
-        shutil.move(f"{today}.png",load_in_path)
-    except:
-        os.remove(load_in_path+f"\\{today}.png")
-        shutil.move(f"{today}.png",load_in_path)
+    w.generate(txt)
+    del_files(load_in_path)
+    w.to_file(f"{yesterday}.png")
+    shutil.move(f"{yesterday}.png",load_in_path)
+    srcFile = f"{load_in_path}\\{yesterday}.png"
+    dstFile = f"{load_in_path}\\{yesterday}-{gid}.png"
+    os.rename(srcFile,dstFile)
+
+
+@sv.on_rex(f'^查询(.*)月(\d+)日词云$')
+async def ciyun(bot, ev: CQEvent):
+    match = ev['match']
+    gid = ev['group_id']
+    month = int(match.group(1))
+    day = int(match.group(2))
+    await bot.send(ev,MessageSegment.image(f'file:///{load_in_path}//2021-{month:02}-{day:02}.png'))
+
+@sv.on_fullmatch('生成今日词云')
+async def getciyun(bot, ev: CQEvent):
+    gid = ev['group_id']
+    if not hoshino.priv.check_priv(ev, hoshino.priv.ADMIN):
+        await bot.send(ev,f"为避免频繁使用刷屏，只能群管理员使用哦~",at_sender = True)
+        return
+    await bot.send(ev,message = '正在生成本群今日词云，请耐心等待',at_sender = True)
+    makeclouds(gid)
+    today = datetime.date.today().__format__('%Y-%m-%d')
+    await bot.send(ev,MessageSegment.image(f'file:///{load_in_path}//{today}-{gid}.png'))
+
+
+@sv.on_fullmatch('生成昨日词云')
+async def getciyunb(bot, ev: CQEvent):
+    gid = ev['group_id']
+    if not hoshino.priv.check_priv(ev, hoshino.priv.ADMIN):
+        await bot.send(ev,f"为避免频繁使用刷屏，只能群管理员使用哦~",at_sender = True)
+        return
+    await bot.send(ev,message = '正在生成本群昨日词云，请耐心等待',at_sender = True)
+    makecloudsb(gid)
+    yesterday = (datetime.date.today() + datetime.timedelta(-1)).__format__('%Y-%m-%d')
+    await bot.send(ev,MessageSegment.image(f'file:///{load_in_path}//{yesterday}-{gid}.png'))
+
+def countFile(dir):
+    tmp = 0
+    for item in os.listdir(dir):
+        if os.path.isfile(os.path.join(dir, item)):
+            tmp += 1
+        else:
+            tmp += countFile(os.path.join(dir, item))
+    return tmp
+
+@sv.on_fullmatch('检查词云')
+async def checkwordcloud(bot, ev: CQEvent):
+    image_api = await bot.can_send_image()
+    image_check = image_api.get('yes')
+    image_all_num = countFile(str(load_in_path))
+    fnnum = image_all_num - 2
+    text = f"【发送权限检查】：\n是否能发送图片:{image_check}\n当前已生成的词云图片数:{fnnum}\n词云背景图片："
+    if not priv.check_priv(ev, priv.SUPERUSER):
+        await bot.send(ev, '权限不足。')
+        return
+    else:
+        await bot.send(ev, text)
+        await bot.send(ev, MessageSegment.image(f'file:///{wd_img}'))
+
+@sv.on_fullmatch(['清除词云','清理词云'])
+async def del_wordcloud(bot, ev: CQEvent):
+    image_all_num = countFile(str(load_in_path))
+    fnnum = image_all_num - 2
+    if not priv.check_priv(ev, priv.SUPERUSER):
+        await bot.send(ev, '权限不足。')
+        return
+    else:
+        if not fnnum == 0:
+            del_files(load_in_path)
+            await bot.send(ev, "词云图片清除成功！")
+        else:
+            await bot.send(ev, "无需清理")
